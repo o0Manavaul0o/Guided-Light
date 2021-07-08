0cb0549c7933e68bd6c74f659212e7584e3d9267
o0Manavaul0o <87138886+o0Manavaul0o@users.noreply.github.com>
2021-07-08 10:28:25 -0500

    Initial Commit

---
+++ Guided Light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Guided Light
+//
+//  Created by Student42 on 7/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
