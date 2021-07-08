e408f18ad1900c9ade317733c34d9de206f43fa7
o0Manavaul0o <87138886+o0Manavaul0o@users.noreply.github.com>
2021-07-08 11:12:35 -0500

    Guided Light Lab
    kinda like a flashlight

--- Guided Light/ViewController.swift
+++ Guided Light/ViewController.swift
@@ -2,18 +2,34 @@
 //  ViewController.swift
 //  Guided Light
 //
-//  Created by Student42 on 7/8/21.
+//  Created by Manav Aul on 7/8/21.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+// Global var and let
+    
+    var lightOn = true
+// Outlets
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    
+// Actions
+    
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn.toggle()
+        
+        if lightOn {
+            view.backgroundColor = .white
+        } else {
+            view.backgroundColor = .black
+        }
+    }
 }
 
